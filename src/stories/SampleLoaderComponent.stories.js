08ab2e55f3d27df2c49ea1dea6eba063a4d153ab
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-06-18 16:34:11 +0100

    Moved component and story to individual props

--- src/stories/SampleLoaderComponent.stories.js
+++ src/stories/SampleLoaderComponent.stories.js
@@ -6,41 +6,12 @@ export default {
   title: "LoaderExample",
 };
 
-// TypeError: Cannot read property 'loaded' of undefined
-// what's missing here
-/* export const SampleStory = (args, { argTypes }, { loaded: { todo } }) => {
-  console.log(`todo:${JSON.stringify(todo, null, 2)}`);
-  return {
-    props: Object.keys(argTypes),
-    components: { SampleLoaderComponent },
-    template: `<SampleLoaderComponent />`,
-  };
-}; */
-
-// using async here returns =>todo:[object Promise]
-/* SampleStory.loaders = [
-  async () => ({
-    todo: (await fetch("https://jsonplaceholder.typicode.com/todos/1")).json(),
-  }),
-]; */
-
-
-
-// this works but how to pass it to the component??
-
-/* Throws this=>Invalid prop: type check failed for prop "todo". Expected Object, got String with value{
-  userId: 0,
-  id: 0,
-  title: "Sanple TODO",
-  completed: false,
-}" */
-// Which is actually not true because type checking the todo it's being reposted as an object
 export const SampleStory = (args, { argTypes, loaded: { todo } }) => {
   console.log(`todo:${JSON.stringify(todo, null, 2)}`);
   return {
     props: Object.keys(argTypes),
     components: { SampleLoaderComponent },
-    template: `<SampleLoaderComponent :todo="todo"/>`,
+    template: `<SampleLoaderComponent :userId="${todo.userId}" :id="${todo.id}" title="${todo.title}" :completed="${todo.completed}"/>`,
   };
 };
 SampleStory.loaders = [
@@ -61,15 +32,11 @@ async function fetchTodo() {
   }
 }
 
-// comes from global loaders (.storybook/preview.js)
-// despite being fetched and logged in the console it throws
-//Property or method "currentUser" is not defined on the instance but referenced during render. Make sure that this property is reactive, either in the data option, or for class-based
-
 export const AnotherStory = (args, { argTypes, loaded: { currentUser } }) => {
   console.log(`current user:${JSON.stringify(currentUser, null, 2)}`);
   return {
     props: Object.keys(argTypes),
     components: { SampleLoaderComponent },
-    template: `<SampleLoaderComponent :todo="currentUser"/>`,
+    template: `<SampleLoaderComponent :userId="${currentUser.userId}" :id="${currentUser.id}" title="${currentUser.title}" :completed="${currentUser.completed}"/>`,
   };
 };
--- src/stories/SampleLoaderComponent.vue
+++ src/stories/SampleLoaderComponent.vue
@@ -2,10 +2,10 @@
   <div>
     <h1>This is a sample component to demonstrate loaders</h1>
     <h2>Todo props has the following inside</h2>
-    <h3>userId:{{ todo.userId }}</h3>
-    <h3>id:{{ todo.id }}</h3>
-    <h3>title:{{ todo.title }}</h3>
-    <h3>completed:{{ todo.completed }}</h3>
+    <h3>userId:{{ userId }}</h3>
+    <h3>id:{{ id }}</h3>
+    <h3>title:{{ title }}</h3>
+    <h3>completed:{{ completed }}</h3>
   </div>
 </template>
 
@@ -13,17 +13,23 @@
 export default {
   name: "sampleloader",
   props: {
-    todo: {
-      type: Object,
-      default: function () {
-        return {
-          userId: 0,
-          id: 0,
-          title: "Sanple TODO",
-          completed: false,
-        };
-      },
+    userId:{
+      type:Number,
+      default:0,
     },
+    id:{
+      type:Number,
+      default:0,
+    },
+    title:{
+      type:String,
+      default:'Sanple TODO',
+    },
+    completed:{
+      type:Boolean,
+      default:false,
+
+    }
   },
 };
 </script>
