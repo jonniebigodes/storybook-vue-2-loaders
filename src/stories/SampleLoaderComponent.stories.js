9521f8383e74cf3e6d6477f695770e8ca3729d3b
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-06-18 16:14:53 +0100

    first commit

---
+++ src/stories/SampleLoaderComponent.stories.js
@@ -0,0 +1,75 @@
+import SampleLoaderComponent from "./SampleLoaderComponent.vue";
+import fetch from "node-fetch";
+
+export default {
+  component: SampleLoaderComponent,
+  title: "LoaderExample",
+};
+
+// TypeError: Cannot read property 'loaded' of undefined
+// what's missing here
+/* export const SampleStory = (args, { argTypes }, { loaded: { todo } }) => {
+  console.log(`todo:${JSON.stringify(todo, null, 2)}`);
+  return {
+    props: Object.keys(argTypes),
+    components: { SampleLoaderComponent },
+    template: `<SampleLoaderComponent />`,
+  };
+}; */
+
+// using async here returns =>todo:[object Promise]
+/* SampleStory.loaders = [
+  async () => ({
+    todo: (await fetch("https://jsonplaceholder.typicode.com/todos/1")).json(),
+  }),
+]; */
+
+
+
+// this works but how to pass it to the component??
+
+/* Throws this=>Invalid prop: type check failed for prop "todo". Expected Object, got String with value{
+  userId: 0,
+  id: 0,
+  title: "Sanple TODO",
+  completed: false,
+}" */
+// Which is actually not true because type checking the todo it's being reposted as an object
+export const SampleStory = (args, { argTypes, loaded: { todo } }) => {
+  console.log(`todo:${JSON.stringify(todo, null, 2)}`);
+  return {
+    props: Object.keys(argTypes),
+    components: { SampleLoaderComponent },
+    template: `<SampleLoaderComponent :todo="todo"/>`,
+  };
+};
+SampleStory.loaders = [
+  async () => ({
+    todo: await fetchTodo(),
+  }),
+];
+
+async function fetchTodo() {
+  try {
+    const fetchRequest = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const response = fetchRequest.json();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// comes from global loaders (.storybook/preview.js)
+// despite being fetched and logged in the console it throws
+//Property or method "currentUser" is not defined on the instance but referenced during render. Make sure that this property is reactive, either in the data option, or for class-based
+
+export const AnotherStory = (args, { argTypes, loaded: { currentUser } }) => {
+  console.log(`current user:${JSON.stringify(currentUser, null, 2)}`);
+  return {
+    props: Object.keys(argTypes),
+    components: { SampleLoaderComponent },
+    template: `<SampleLoaderComponent :todo="currentUser"/>`,
+  };
+};
---
+++ src/stories/SampleLoaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>This is a sample component to demonstrate loaders</h1>
+    <h2>Todo props has the following inside</h2>
+    <h3>userId:{{ todo.userId }}</h3>
+    <h3>id:{{ todo.id }}</h3>
+    <h3>title:{{ todo.title }}</h3>
+    <h3>completed:{{ todo.completed }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sampleloader",
+  props: {
+    todo: {
+      type: Object,
+      default: function () {
+        return {
+          userId: 0,
+          id: 0,
+          title: "Sanple TODO",
+          completed: false,
+        };
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
